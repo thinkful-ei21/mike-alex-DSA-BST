c78279f85422cb07f8f8bac6e2dd57978cc94f80
Michael Le-Reiver <m.lereiver@gmail.com>
2018-07-31 12:30:59 -0700

    completed Height of BST

--- index.js
+++ index.js
@@ -1,160 +1,215 @@
 class BinarySearchTree {
-    constructor(key=null, value=null, parent=null) {
-        this.key = key;
-        this.value = value;
-        this.parent = parent;
+  constructor(key=null, value=null, parent=null) {
+    this.key = key;
+    this.value = value;
+    this.parent = parent;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(key, value) {
+    //if the tree is empty then this key being inserted is the root node of the tree
+    if (this.key == null) {
+      this.key = key;
+      this.value = value;
+    }
+
+    //If the tree already exist, then start at the root, 
+    //and compare it to the key you want to insert
+    // If the new key is less than the node's key 
+    //then the new node needs to live in the left-hand branch.
+    else if (key < this.key) {
+      //if the existing node does not have any left child, 
+      //meaning that if the `left` pointer is empty 
+      //then we can just instantiate and insert the new node 
+      //as the left child of that node, passing `this` as the parent.  
+      if (this.left == null) {
+        this.left = new BinarySearchTree(key, value, this);
+      }
+      //if the node has an existing left child, 
+      //then we recursively call the `insert` method 
+      //so the node is added further down the tree.
+      else {
+        this.left.insert(key, value);
+      }
+    }
+    //Similarly, if the new key is greater than the node's key 
+    //then you do the same thing, but on the right-hand side.
+    else {
+      if (this.right == null) {
+        this.right = new BinarySearchTree(key, value, this);
+      }
+      else {
+        this.right.insert(key, value);
+      }
+    }
+  }
+
+  find(key) {
+    //if the item is found at the root then return that value
+    if (this.key == key) {
+      return this.value;
+    }
+    //if the item you are looking for is less than the root 
+    //then follow the left child
+    //if there is an existing left child, 
+    //then recursively check its left and/or right child
+    //until you find the item.
+    else if (key < this.key && this.left) {
+      return this.left.find(key);
+    }
+    //if the item you are looking for is greater than the root 
+    //then follow the right child
+    //if there is an existing right child, 
+    //then recursively check its left and/or right child
+    //until you find the item.
+    else if (key > this.key && this.right) {
+      return this.right.find(key);
+    }
+    //You have search the tree and the item is not in the tree
+    else {
+      throw new Error('Key Error');
+    }
+  }
+
+  remove(key) {
+    if (this.key == key) {
+      if (this.left && this.right) {
+        const successor = this.right._findMin();
+        this.key = successor.key;
+        this.value = successor.value;
+        successor.remove(successor.key);
+      }
+      //If the node only has a left child, 
+      //then you replace the node with its left child.  
+      else if (this.left) {
+        this._replaceWith(this.left);
+      }
+      //And similarly if the node only has a right child 
+      //then you replace it with its right child.
+      else if (this.right) {
+        this._replaceWith(this.right);
+      }
+      //If the node has no children then
+      //simply remove it and any references to it 
+      //by calling "this._replaceWith(null)".
+      else {
+        this._replaceWith(null);
+      }
+    }
+    else if (key < this.key && this.left) {
+      this.left.remove(key);
+    }
+    else if (key > this.key && this.right) {
+      this.right.remove(key);
+    }
+    else {
+      throw new Error('Key Error');
+    }
+  }
+
+  _replaceWith(node) {
+    if (this.parent) {
+      if (this == this.parent.left) {
+        this.parent.left = node;
+      }
+      else if (this == this.parent.right) {
+        this.parent.right = node;
+      }
+
+      if (node) {
+        node.parent = this.parent;
+      }
+    }
+    else {
+      if (node) {
+        this.key = node.key;
+        this.value = node.value;
+        this.left = node.left;
+        this.right = node.right;
+      }
+      else {
+        this.key = null;
+        this.value = null;
         this.left = null;
         this.right = null;
+      }
     }
+  }
 
-    insert(key, value) {
-        //if the tree is empty then this key being inserted is the root node of the tree
-        if (this.key == null) {
-            this.key = key;
-            this.value = value;
-        }
-
-        //If the tree already exist, then start at the root, 
-        //and compare it to the key you want to insert
-        // If the new key is less than the node's key 
-        //then the new node needs to live in the left-hand branch.
-        else if (key < this.key) {
-            //if the existing node does not have any left child, 
-            //meaning that if the `left` pointer is empty 
-            //then we can just instantiate and insert the new node 
-            //as the left child of that node, passing `this` as the parent.  
-            if (this.left == null) {
-                this.left = new BinarySearchTree(key, value, this);
-            }
-            //if the node has an existing left child, 
-            //then we recursively call the `insert` method 
-            //so the node is added further down the tree.
-            else {
-                this.left.insert(key, value);
-            }
-        }
-        //Similarly, if the new key is greater than the node's key 
-        //then you do the same thing, but on the right-hand side.
-        else {
-            if (this.right == null) {
-                this.right = new BinarySearchTree(key, value, this);
-            }
-            else {
-                this.right.insert(key, value);
-            }
-        }
+  _findMin() {
+    if (!this.left) {
+      return this;
     }
+    return this.left._findMin();
+  }
 
-    find(key) {
-        //if the item is found at the root then return that value
-        if (this.key == key) {
-            return this.value;
-        }
-        //if the item you are looking for is less than the root 
-        //then follow the left child
-        //if there is an existing left child, 
-        //then recursively check its left and/or right child
-        //until you find the item.
-        else if (key < this.key && this.left) {
-            return this.left.find(key);
-        }
-        //if the item you are looking for is greater than the root 
-        //then follow the right child
-        //if there is an existing right child, 
-        //then recursively check its left and/or right child
-        //until you find the item.
-        else if (key > this.key && this.right) {
-            return this.right.find(key);
-        }
-        //You have search the treen and the item is not in the tree
-        else {
-            throw new Error('Key Error');
-        }
+  _findMax() {
+    if (!this.right) {
+      return this;
     }
+    return this.right._findMax();
+  }
 
-    remove(key) {
-        if (this.key == key) {
-            if (this.left && this.right) {
-                const successor = this.right._findMin();
-                this.key = successor.key;
-                this.value = successor.value;
-                successor.remove(successor.key);
-            }
-            //If the node only has a left child, 
-            //then you replace the node with its left child.  
-            else if (this.left) {
-                this._replaceWith(this.left);
-            }
-            //And similarly if the node only has a right child 
-            //then you replace it with its right child.
-            else if (this.right) {
-                this._replaceWith(this.right);
-            }
-            //If the node has no children then
-            //simply remove it and any references to it 
-            //by calling "this._replaceWith(null)".
-            else {
-                this._replaceWith(null);
-            }
-        }
-        else if (key < this.key && this.left) {
-            this.left.remove(key);
-        }
-        else if (key > this.key && this.right) {
-            this.right.remove(key);
-        }
-        else {
-            throw new Error('Key Error');
-        }
-    }
-
-    _replaceWith(node) {
-        if (this.parent) {
-            if (this == this.parent.left) {
-                this.parent.left = node;
-            }
-            else if (this == this.parent.right) {
-                this.parent.right = node;
-            }
-
-            if (node) {
-                node.parent = this.parent;
-            }
-        }
-        else {
-            if (node) {
-                this.key = node.key;
-                this.value = node.value;
-                this.left = node.left;
-                this.right = node.right;
-            }
-            else {
-                this.key = null;
-                this.value = null;
-                this.left = null;
-                this.right = null;
-            }
-        }
-    }
-
-    _findMin() {
-        if (!this.left) {
-            return this;
-        }
-        return this.left._findMin();
-    }
-
+ 
 }
 
 function main() {
-    let bst = new BinarySearchTree()
-    const arr = [[3,3],[1,1],[4,4],[6,6],[9,9],[2,2],[5,5],[7,7]]
-    
-    arr.forEach(element => {
-        bst.insert(element[0], element[1])
-    });
-    console.log(bst)
-    console.log(bst.find(9))
-}
+  let bst = new BinarySearchTree()
+  const arr = [[3,'a'],[1,'b'],[4,'c'],[6,'d'],[9,'e'],[2,'f'],[5,'f'],[7,'g']];
 
-main()
+  arr.forEach(element => {
+    bst.insert(element[0], element[1]);
+  });
+
+  // console.log(bst.right._findMin());
+  // console.log(bst.find(9))
+  // console.log(bst.remove(5));
+  // console.log(bst._bstHeight(bst));
+  // console.log(bst.right.right.right.left);
+}
+main();
+
+
+let bst = new BinarySearchTree();
+const arr = [[3,'a'],[1,'b'],[4,'c'],[6,'d'],[9,'e'],[2,'f'],[5,'f'],[7,'g']];
+
+arr.forEach(element => {
+  bst.insert(element[0], element[1]);
+});
+
+const bstHeight = (bst, level=1)  => {
+
+  // create base case
+  if ( bst.left === null && bst.right === null) {
+    return level;
+  }
+
+  // start at root
+  // go to left 
+
+  // level count ++
+  level++;
+  // then call function on this.right
+  if(bst.left === null) {
+    return bstHeight(bst.right, level);
+  }
+  // call function on this.left, 
+  if(bst.right === null) {
+    return bstHeight(bst.left, level);
+  }
+
+  // call both left and then right recursively
+  return Math.max(bstHeight(bst.left, level), bstHeight(bst.right, level));
+
+};
+
+console.log(bstHeight(bst));
+
+const thirdLargest = () => {
+// look left and right
+
+// 
+
+};
+
